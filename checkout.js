44c43cc0f4d6e98c506585bc14df1523f700d031
Rob <rob.smeitink@gmail.com>
2024-04-25 10:28:52 +0200

    Bugfix

--- checkout.js
+++ checkout.js
@@ -1,42 +1,44 @@
 document.body.addEventListener("click", async (event) => {
-    const targetElement = event.target;
-  
-    if (targetElement.closest('[wized="checkout_button"]')) {
-      const storedProducts = JSON.parse(
-        localStorage.getItem(window.LOCAL_STORAGE_KEY) || "[]"
-      );
+  const targetElement = event.target;
 
-      const lineItems = storedProducts.map((product) => {
-        return {
-          variantId: product.productVariantId,
-          quantity: product.quantity,
-          sellingPlanId: product.sellingPlanId
-        };
-      });
-  
-      // Convert to the GraphQL format
-      const formattedLineItems = lineItems
-        .map(
-          (item) => item.sellingPlanId ? `{merchandiseId: "${item.variantId}", quantity: ${item.quantity}, sellingPlanId: "${item.sellingPlanId}"}` : `{merchandiseId: "${item.variantId}", quantity: ${item.quantity}}`
-        )
-        .join(", ");
-  
-      // Set the Wized variable
-      Wized.data.v.checkoutItems = `[${formattedLineItems}]`;
+  if (targetElement.closest('[wized="checkout_button"]')) {
+    const storedProducts = JSON.parse(
+      localStorage.getItem(window.LOCAL_STORAGE_KEY) || "[]",
+    );
 
-      await Wized.request.execute("get_auth_user");
-  
-      // Execute the Wized request
-      await Wized.requests.execute("create_shopify_otc_checkout");
-  
-      // Get the checkout URL
-      const checkoutURL = Wized.data.r.create_shopify_otc_checkout.data.data.cartCreate.cart.checkoutUrl;
-  
-      // Redirect the user to the checkout URL
-      if (checkoutURL) {
-        window.location.href = checkoutURL;
-      } else {
-        console.error("Failed to retrieve checkout URL");
-      }
+    const lineItems = storedProducts.map((product) => {
+      return {
+        price_id: product.stripe_price_id,
+        quantity: product.quantity,
+      };
+    });
+
+    // Check if any item in storedProducts has mode === 'subscription'
+    const hasSubscription = storedProducts.some(
+      (product) => product.mode === "subscription",
+    );
+    let checkoutMode = hasSubscription ? "subscription" : "payment";
+
+    // Set the Wized variable
+    Wized.data.v.checkoutItems = lineItems;
+    Wized.data.v.checkoutMode = checkoutMode;
+
+    // Get Auth User To Prefill Customer Data If Logged In
+    await Wized.requests.execute("get_session");
+    await Wized.requests.execute("get_auth_user");
+
+    // Execute the Wized request
+    await Wized.requests.execute("create_stripe_otc_checkout_session");
+
+    // Get the checkout URL
+    const checkoutURL =
+      Wized.data.r.create_stripe_otc_checkout_session.data.session_url;
+
+    // Redirect the user to the checkout URL
+    if (checkoutURL) {
+      window.location.href = checkoutURL;
+    } else {
+      console.error("Failed to retrieve checkout URL");
     }
-  });
+  }
+});
